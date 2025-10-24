0569b7d0b5aea4cf46e6d0062c31137dc777d61c
Arrokoth233 <arrokoth233@gmail.com>
2025-10-24 12:57:13 +0200

    Initialize

---
+++ src/main/java/team/dovecotmc/kamera/Kamera.java
@@ -0,0 +1,10 @@
+package team.dovecotmc.kamera;
+
+import net.fabricmc.api.ModInitializer;
+
+public class Kamera implements ModInitializer {
+
+    @Override
+    public void onInitialize() {
+    }
+}
---
+++ src/main/java/team/dovecotmc/kamera/client/KameraClient.java
@@ -0,0 +1,10 @@
+package team.dovecotmc.kamera.client;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class KameraClient implements ClientModInitializer {
+
+    @Override
+    public void onInitializeClient() {
+    }
+}
